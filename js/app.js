7d3741c1aab94d93f1972a5b979ead477d27ff9c
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-27 15:50:39 -0300

    programmed nav btns and styled btns

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const showDisplay = (e) => {
   try {
+
     const menu = e.target.querySelector('ul');
     menu.classList.toggle('show');
     menu.classList.toggle('hide');
+
+    e.target.querySelector('img').classList.toggle('flip');
   } catch (error) {
     
   }
