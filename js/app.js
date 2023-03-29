edbfc6f66c77356ed53ef9122d79b15717a1f4cf
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-28 23:36:19 -0300

    finished mobile styles

--- js/app.js
+++ js/app.js
@@ -11,7 +11,28 @@ const showDisplay = (e) => {
   }
 }
 
+const showMobileMenu = (e) => {
+  try {
+    const closeIcon = './images/icon-close.svg';
+    const openIcon = './images/icon-hamburger.svg';
+    
+    if(e.target.src.split('/')[e.target.src.split('/').length - 1] == 'icon-hamburger.svg') {
+      e.target.src = closeIcon;
+    }
+    else {
+      e.target.src = openIcon;
+    }
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('desktopMenu');
+  } catch (error) {
+    
+  }
+}
+
 const menus = document.querySelectorAll('.headerMenu');
 menus.forEach(menu => {
   menu.addEventListener('click', showDisplay);
 })
+
+const hamBtn = document.querySelector('#hamMenu');
+hamBtn.addEventListener('click', showMobileMenu);
